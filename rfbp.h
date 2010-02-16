072e048b2651dc9419ae1658eab3e6042092dfb1
agcc <agcc@agcc-laptop.(none)>
2010-02-15 20:31:41 -0600

    new files for tcp and header file

---
+++ rfbp.h
@@ -0,0 +1,30 @@
+#ifndef RFBP_H
+#define RFBP_H
+
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <pthread.h>
+#include <errno.h>
+#include <signal.h>
+#include <syslog.h>
+#include <netdb.h>
+#include <fcntl.h>
+#include <sys/time.h>
+#include <string.h>
+#include <arpa/inet.h>
+#include <sys/stat.h>
+#include <sys/wait.h>
+#include <signal.h>
+#include <ctype.h>
+#include <getopt.h>
+
+
+#include <cstdlib>
+
+#define bzero(ptr,n) memset(ptr, 0, n)
+
+#endif
